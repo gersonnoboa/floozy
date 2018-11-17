612bf63ad83b17499dbdc802fefb9c45edbfc6cd
Gerson Noboa <gnoboa@protonmail.com>
2018-11-17 20:39:32 +0200

    Added material, navigation bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AlbumLengthComponent } from './album-length/album-length.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: "album-length",
+        component: AlbumLengthComponent
+    },
+    {
+        path: "",
+        redirectTo: "/album-length",
+        pathMatch: "full"
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlbumLengthComponent } from './album-length/album-length.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlbumLengthComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
